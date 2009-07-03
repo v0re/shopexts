f3ac5844a3d56a5503780e67fcaef5a7daf113f9
alonelife86@gmail.com <alonelife86@gmail.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2009-07-03 02:30:44 +0000

    遍历目录下的所有文件 使用glob

--- branches/fzl/showtree.php
+++ branches/fzl/showtree.php
@@ -1,19 +1,28 @@
 <?php
 
-	
+	/**
+	 *	@get_all_files
+	 *	@param: string $path	目录名
+	 *	@功能 遍历目录及子目录下的文件
+	 *	@return array $list 存放目录下的所有文件
+	 */
 	function get_all_files( $path )
 	{
 		$list = array();
+		//$path = (substr($path,-1,1) == '/')?substr($path,0,strlen($path)-1):$path;
 		foreach( glob( $path . '/*') as $item )
 		{
+			//若是目录,递归处理
 			if( is_dir( $item ) )
 			{
-			 $list = array_merge( $list , get_all_files( $item ) );
-			 //error_log(print_r($list,true),3,__FILE__.'.log');
+				//将子目录中的文件合并到$list数组
+				$list = array_merge( $list , get_all_files( $item ) );
+				//error_log(print_r($list,true),3,__FILE__.'.log');
 			}
 			else
 			{
-			 $list[] = $item;
+				//不是将给定目录下文件存入数组$list
+				$list[] = $item;
 			}
 		}
     return $list;
