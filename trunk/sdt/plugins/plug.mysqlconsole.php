07bb75cd0b22540728b5b8570c259d2e5074a44b
xuqinyong <xuqinyong@8caafd3e-42ec-11de-b17b-0148c022b60b>
2009-07-11 18:16:28 +0000

    完成MySQL控制台查询功能

--- trunk/sdt/plugins/plug.mysqlconsole.php
+++ trunk/sdt/plugins/plug.mysqlconsole.php
@@ -4,14 +4,50 @@ class mysqlconsole {
 	var $label = 'MySQL控制台';
 
 	var $sql;
+	var $logfile;
+
+	function mysqlconsole() {
+		$this->logfile = MEAT.'/log/sql.log';
+	}
 
 	function run(){
 		switch($_REQUEST['action']){
 
 			case "query":
-				$this->sql = $_POST['sql'];
-				$this->showForm();				
-				mysql_query($this->sql);
+				$this->sql = trim($_POST['sql']);
+				if(get_magic_quotes_gpc()){
+					$this->sql = stripslashes($this->sql);
+				}
+				$this->showForm();		
+				$this->wlog();
+				$rs = mysql_query($this->sql) or die(mysql_error());
+				if( in_array(strtolower(substr($this->sql,0,4)),array('sele','desc','show')) ){
+					echo "<p><table border=1>";
+					$i = 0;
+					while($row = mysql_fetch_array( $rs, MYSQL_ASSOC )){
+						if($i == 0 ){
+							echo "<tr>";
+							foreach(array_keys($row) as $val){
+								echo "<td>".$val."</td>";
+							}	
+							echo "<tr>";
+						}
+
+						echo "<tr>";
+						foreach($row as $val){
+								echo "<td>".htmlspecialchars($val)."</td>";			
+						}
+						echo "</tr>";	
+						$i++;		
+					}
+					echo "</table></p>";
+				}else{
+					if($rs){
+						echo "命令执行成功";
+					}else{
+						echo "命令执行失败<br><p>".mysql_error()."</p>";
+					}
+				}
 			break;
 			
 			default:
@@ -21,7 +57,10 @@ class mysqlconsole {
 	}
 
 	function showForm() {
+
+		$mysqluri = "mysql://".DB_USER.":".DB_PASSWORD."@".DB_HOST.":".DB_PORT."/".DB_NAME;
 		echo <<<EOF
+当前位置:{$mysqluri}
 <form method=post>
 <input type=hidden name=module value=mysqlconsole>
 <input type=hidden name=action value=query>
@@ -34,6 +73,15 @@ class mysqlconsole {
 EOF;
 	}
 
+	function wlog(){
+		$log = $this->sql;
+		$log = $log."\r\n";
+		error_log($log,3,$this->logfile);
+	}
+
+	function rlog(){
+		
+	}
 }
 
 ?>
