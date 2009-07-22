b4d1fce2ceac44d610061c865ca077b4d00e0e1d
xuqinyong <xuqinyong@8caafd3e-42ec-11de-b17b-0148c022b60b>
2009-07-22 05:31:22 +0000

    添加仿京东商品分类

---
+++ shopex48/front/360buygoodscat/plugins/widgets/goodscat/widgets.php
@@ -0,0 +1,24 @@
+<?php
+
+$setting['author']='';
+$setting['name']='商品分类';
+$setting['version']='135711';
+
+$setting['stime']='2008-8-8';
+//,product,goods:act,
+//$setting['scope']=array('');
+$setting['order']='1';
+$setting['catalog']='商品相关';
+
+$setting['description']    = '多种分类展示形式。';
+
+$setting['usual']    = '1';
+
+$setting['template'] = array(
+                            'default.html'=>'默认',
+                            'toggle.html'=>'折叠式',
+                            'dropdown.html'=>'弹出式',
+														'floatright.html'=>'右浮动',
+                            
+                        );
+?>
