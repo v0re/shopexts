3cafd2931e134c533ebbe89e11c6ce2835c0c8ee
csw429714@yahoo.com.cn <csw429714@yahoo.com.cn@8caafd3e-42ec-11de-b17b-0148c022b60b>
2009-09-11 06:16:10 +0000

    huil

---
+++ branches/csw/change/widgets.php
@@ -0,0 +1,25 @@
+
+<?php
+
+
+$setting['author']='ccdsw';
+
+$setting['name']='外币汇率';
+
+$setting['version']='1.0';
+
+$setting['description']    = '外币兑换';
+
+
+$setting['catalog']='自定义板块';
+
+$setting['usual']    = '1';
+
+$setting['stime']='2009-8-24';
+
+$setting['template'] = array(
+                            'default.html'=>'默认'                          
+                        );
+
+?>
+
