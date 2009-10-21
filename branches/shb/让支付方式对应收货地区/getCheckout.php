9403af50860c235e17e905aaf9374ede8b459136
sanow@126.com <sanow@126.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2009-10-21 05:04:05 +0000

    添加让支付方式与地区关联的部分代码： 具体操作请访问：https://docs.google.com/a/shopcare.net/Doc?docid=0AYsxrJm0E_WJZGdicXh3dmpfMTk0cTdjc2RjMw&hl=zh_CN

---
+++ branches/shb/让支付方式对应收货地区/getCheckout.php
@@ -0,0 +1,45 @@
+﻿ function getCheckout(&$aCart, $aMember, $currency,$areaid){###################################增加一个参数（具体地区的ID）
+	
+        $trading = $this->checkoutInfo($aCart, $aMember);
+        $gtype = $this->system->loadModel('goods/gtype');
+        foreach($trading['products'] as $p){
+            if($p['goods_id'] > 0) $aP[] = $p['goods_id'];
+        }
+        if(!empty($aP) || $trading['package'] || $trading['gift_e']){    //todo 需要处理捆绑商品的配送问题
+            if(!empty($aP)){
+                $deliverInfo = $gtype->deliveryInfo($aP);
+                $aOut['has_physical'] = $deliverInfo['physical'];
+            }else{
+                $aOut['has_physical'] = 1;
+            }
+            foreach($trading['products'] as $product){
+                if($deliverInfo['custom'][$product['type_id']]){
+                    $aOut['minfo'][$product['product_id']] = array(
+                            'goods_id' => $product['goods_id'],
+                            'nums' => $product['nums'],
+                            'name' => $product['name'],
+                            'minfo' => &$deliverInfo['custom'][$product['type_id']]);
+                }
+            }
+            $oDly = $this->system->loadModel('trading/delivery');
+            if($area = $oDly->getDlAreaList()){
+                foreach($area as $a){
+                    $aOut['areas'][$a['area_id']]=$a['name']; 
+                }
+            }
+        }
+        
+        $payment = $this->system->loadModel('trading/payment');
+        $oCur = $this->system->loadModel('system/cur');
+        $currency = $oCur->getcur($currency, true);
+
+        $oMem = $this->system->loadModel('member/member');
+        $trading['receiver'] = $oMem->getDefaultAddr($aMember['member_id']);
+        $trading['receiver']['email'] = $aMember['email'];
+
+        $aOut['currencys'] = $oCur->curAll();
+        $aOut['currency'] = $currency['cur_code'];
+        $aOut['payments'] = $payment->getByCur($currency['cur_code'],$type,$areaid);###################增加一个参数（具体地区的ID）
+        $aOut['trading'] = $trading;
+        return $aOut;
+    }
---
+++ branches/shb/让支付方式对应收货地区/shipping.php
@@ -0,0 +1,27 @@
+﻿   function shipping(){
+        $sale = $this->system->loadModel('trading/sale');
+        $trading = $sale->getCartObject($this->cart,$GLOBALS['runtime']['member_lv'],true);
+        $shipping = $this->system->loadModel('trading/delivery');
+        $aShippings = $shipping->getDlTypeByArea($_POST['area']);
+        foreach($aShippings as $k=>$s){
+            $aShippings[$k]['price'] = cal_fee($s['expressions'],$trading['weight'],$trading['pmt_b']['totalPrice'],$s['price']);
+            $s['pad']==0?$aShippings[$k]['has_cod'] = 0:$aShippings[$k]['has_cod'] = 1;
+            if($s['protect']==1){
+                $aShippings[$k]['protect'] = true;
+            }else{
+                $aShippings[$k]['protect'] = false;
+            }
+        }
+		################################################################################################################
+		$aOut = $this->objCart->getCheckout($this->cart, $this->member, $this->system->request['cur'],$_POST['area']);//增加一个参数是收获地区的ID
+		
+        $payment = $this->system->loadModel('trading/payment');
+		
+        $payment->showPayExtendCon($aOut['payments']);
+        $this->pagedata['payments'] = $aOut['payments'];//支付方式
+		###############################################################################################################
+        $this->pagedata['shippings'] = $aShippings;
+        $this->__tmpl='cart/checkout_shipping.html';
+        $this->output();
+		
+    }
