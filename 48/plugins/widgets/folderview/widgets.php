73efafdfcb8d1a443828fcbbb891cf077a1c07d4
xuqinyong <xuqinyong@8caafd3e-42ec-11de-b17b-0148c022b60b>
2010-02-27 05:28:57 +0000

    目录浏览

---
+++ 48/plugins/widgets/folderview/widgets.php
@@ -0,0 +1,23 @@
+<?php
+
+$setting['author']='KenXu';
+
+$setting['version']='135711';
+$setting['order']=18;
+
+
+$setting['name']='自定义板块-目录管理器';
+
+$setting['catalog']    = '自定义板块';
+
+$setting['description']    = '本挂件用于管理下载目录文件';
+
+$setting['usual']    = '0';
+
+$setting['stime']='2008-8-8';
+
+$setting['template'] = array(
+                            'default.html'=>'默认'
+                        );
+
+?>
