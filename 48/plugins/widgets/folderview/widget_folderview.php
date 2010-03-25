e3c75f9847522a5bbe12b1d103cf7178e0e21b3c
xuqinyong@gmail.com <xuqinyong@gmail.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2010-03-25 07:23:09 +0000

    新增排序功能

--- 48/plugins/widgets/folderview/widget_folderview.php
+++ 48/plugins/widgets/folderview/widget_folderview.php
@@ -1,7 +1,7 @@
 <?php
 function widget_folderview(&$setting,&$system){
 	file_put_contents(dirname(__FILE__)."/setting.conf",serialize($setting));
-	$dir = BASE_DIR."/config";
+	$dir = realpath(BASE_DIR."/home/shujubao/");
 	$d = dir($dir);
 	$i=1;
 	while (false !== ($entry = $d->read())) {
@@ -9,13 +9,28 @@ function widget_folderview(&$setting,&$system){
 		$name = "$dir/$entry";
 		$lastmodify = filemtime($name);
 		$lastmodify = date('Y-m-d H:i:s',$lastmodify);
-		$url = $system->base_url().str_replace(BASE_DIR."/",'',$dir)."/$entry";
+		$url = $system->base_url().str_replace(realpath(BASE_DIR)."/",'',$dir)."/$entry";
 		$alias = $setting['alias'][$entry] ? $setting['alias'][$entry] : $entry;
 		$aFileList[$i] = array('url'=>$url,'name'=>$name,'alias'=>$alias,'lastmodify'=>$lastmodify);		
 		$i++;
 	}
 	$d->close();
+    $aFileList = m_array_sort($aFileList,'lastmodify');
     $data['files'] = $aFileList;
     return $data;
 }
+
+function m_array_sort(&$array,$sortkey){
+	foreach($array as $key=>$row){
+		$keyvalue[$key] = $row[$sortkey];
+	}
+	arsort($keyvalue);
+	foreach($keyvalue as $key=>$value){
+		$ret[$key] = $array[$key];
+	}
+
+	return $ret;
+}
+
+
 ?>
