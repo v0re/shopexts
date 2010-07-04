a488f41e331bbfec3f93774dd1fec7cfa43e34fc
xuqinyong@gmail.com <xuqinyong@gmail.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2010-07-04 07:37:11 +0000

    update tiny.php

--- security/crypt/rsa/sample/tiny.php
+++ security/crypt/rsa/sample/tiny.php
@@ -5,22 +5,22 @@ $q = 59;
 $n = $p * $q; #modulus
 $t = ($p - 1) * ($q -1 );
 $e = 63; #public key
-#find the private key $d
-for($d=9999;$d>0;$d--){
-	if ($d * $e % $t == 1) break;
+#find the private key $d ,we use Euclidean algorithm to make it see detail in 
+#http://blog.csdn.net/xuanshayuan/archive/2008/03/14/2181807.aspx
+for($d=2;$d<$t;$d++){
+	if (bcmod($d  * $e, $t) == 1) break;
 }
-$d = 847; #there is something wrong with $d cuculation
 $m = 244;
 print "modulus n = $n\n";
-print "public key d = $d\n";
-print "private key e = $e\n";
+print "public key e = $e\n";
+print "private key d = $d\n";
 print "message m = $m\n";
 
-$encrypt_msg = bcpowmod($m,$d,$n);
+$encrypt_msg = bcpowmod($m,$d,$n); #use private key $d and modulus $n for encrypt,take $d secret
 
 print "encrtyp message : $encrypt_msg \n";
 
-$decrypt_msg = bcpowmod($encrypt_msg,$e,$n);
+$decrypt_msg = bcpowmod($encrypt_msg,$e,$n); # use public key $e and modulus $e for decrypt,$e can be publish
 
 print "decrtyp message : $decrypt_msg \n";
 
