eb8a2dd714ae37801672238c996f48c2e3cca7e0
xuqinyong@gmail.com <xuqinyong@gmail.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2010-07-05 07:05:17 +0000

    add oninone model

--- security/crypt/rsa/datasafe/protype/decrypt.php
+++ security/crypt/rsa/datasafe/protype/decrypt.php
@@ -5,6 +5,8 @@ $fp=fopen("privkey","r");
 $priv_key=fread($fp,8192);
 fclose($fp);
 $passphrase = null;
+var_dump(base64_encode($priv_key));
+die();
 $res = openssl_get_privatekey($priv_key,$passphrase);
 openssl_private_decrypt($crypttext,$newsource,$res);
 echo $newsource;
