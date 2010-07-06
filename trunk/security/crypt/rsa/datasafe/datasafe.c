ce79c0ea0512efc7274a54e2232cc952802cb230
xuqinyong <xuqinyong@8caafd3e-42ec-11de-b17b-0148c022b60b>
2010-07-06 18:54:17 +0000

    增加openssl rsa加密解密原型

--- trunk/security/crypt/rsa/datasafe/datasafe.c
+++ trunk/security/crypt/rsa/datasafe/datasafe.c
@@ -90,7 +90,7 @@ enum php_openssl_cipher_type {
     PHP_OPENSSL_CIPHER_DEFAULT = PHP_OPENSSL_CIPHER_RC2_40
 };
 
-
+static EVP_PKEY * php_openssl_evp_from_zval(zval ** val, int public_key, char * passphrase, int makeresource, long * resourceval TSRMLS_DC);
 /* If you declare any globals in php_datasafe.h uncomment this:
 ZEND_DECLARE_MODULE_GLOBALS(datasafe)
 */
@@ -230,6 +230,178 @@ PHP_FUNCTION(confirm_datasafe_compiled)
 	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "datasafe", arg);
 	RETURN_STRINGL(strg, len, 0);
 }
+
+/* {{{ php_openssl_evp_from_zval
+   Given a zval, coerce it into a EVP_PKEY object.
+	It can be:
+		1. private key resource from openssl_get_privatekey()
+		2. X509 resource -> public key will be extracted from it
+		3. if it starts with file:// interpreted as path to key file
+		4. interpreted as the data from the cert/key file and interpreted in same way as openssl_get_privatekey()
+		5. an array(0 => [items 2..4], 1 => passphrase)
+		6. if val is a string (possibly starting with file:///) and it is not an X509 certificate, then interpret as public key
+	NOTE: If you are requesting a private key but have not specified a passphrase, you should use an
+	empty string rather than NULL for the passphrase - NULL causes a passphrase prompt to be emitted in
+	the Apache error log!
+*/
+static EVP_PKEY * php_openssl_evp_from_zval(zval ** val, int public_key, char * passphrase, int makeresource, long * resourceval TSRMLS_DC)
+{
+	EVP_PKEY * key = NULL;
+	X509 * cert = NULL;
+	int free_cert = 0;
+	long cert_res = -1;
+	char * filename = NULL;
+	zval tmp;
+
+	Z_TYPE(tmp) = IS_NULL;
+
+#define TMP_CLEAN \
+	if (Z_TYPE(tmp) == IS_STRING) {\
+		zval_dtor(&tmp); \
+	} \
+	return NULL;
+
+	if (resourceval) {
+		*resourceval = -1;
+	}
+	if (Z_TYPE_PP(val) == IS_ARRAY) {
+		zval ** zphrase;
+		
+		/* get passphrase */
+
+		if (zend_hash_index_find(HASH_OF(*val), 1, (void **)&zphrase) == FAILURE) {
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "key array must be of the form array(0 => key, 1 => phrase)");
+			return NULL;
+		}
+		
+		if (Z_TYPE_PP(zphrase) == IS_STRING) {
+			passphrase = Z_STRVAL_PP(zphrase);
+		} else {
+			tmp = **zphrase;
+			zval_copy_ctor(&tmp);
+			convert_to_string(&tmp);
+			passphrase = Z_STRVAL(tmp);
+		}
+
+		/* now set val to be the key param and continue */
+		if (zend_hash_index_find(HASH_OF(*val), 0, (void **)&val) == FAILURE) {
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "key array must be of the form array(0 => key, 1 => phrase)");
+			TMP_CLEAN;
+		}
+	}
+
+	if (Z_TYPE_PP(val) == IS_RESOURCE) {
+		void * what;
+		int type;
+
+		what = zend_fetch_resource(val TSRMLS_CC, -1, "OpenSSL X.509/key", &type, 2, le_x509, le_key);
+		if (!what) {
+			TMP_CLEAN;
+		}
+		if (resourceval) { 
+			*resourceval = Z_LVAL_PP(val);
+		}
+		if (type == le_x509) {
+			/* extract key from cert, depending on public_key param */
+			cert = (X509*)what;
+			free_cert = 0;
+		} else if (type == le_key) {
+			int is_priv;
+
+			is_priv = php_openssl_is_private_key((EVP_PKEY*)what TSRMLS_CC);
+
+			/* check whether it is actually a private key if requested */
+			if (!public_key && !is_priv) {
+				php_error_docref(NULL TSRMLS_CC, E_WARNING, "supplied key param is a public key");
+				TMP_CLEAN;
+			}
+
+			if (public_key && is_priv) {
+				php_error_docref(NULL TSRMLS_CC, E_WARNING, "Don't know how to get public key from this private key");
+				TMP_CLEAN;
+			} else {
+				if (Z_TYPE(tmp) == IS_STRING) {
+					zval_dtor(&tmp);
+				}
+				/* got the key - return it */
+				return (EVP_PKEY*)what;
+			}
+		} else {
+			/* other types could be used here - eg: file pointers and read in the data from them */
+			TMP_CLEAN;
+		}
+	} else {
+		/* force it to be a string and check if it refers to a file */
+		/* passing non string values leaks, object uses toString, it returns NULL 
+		 * See bug38255.phpt 
+		 */
+		if (!(Z_TYPE_PP(val) == IS_STRING || Z_TYPE_PP(val) == IS_OBJECT)) {
+			TMP_CLEAN;
+		}
+		convert_to_string_ex(val);
+
+		if (Z_STRLEN_PP(val) > 7 && memcmp(Z_STRVAL_PP(val), "file://", sizeof("file://") - 1) == 0) {
+			filename = Z_STRVAL_PP(val) + (sizeof("file://") - 1);
+		}
+		/* it's an X509 file/cert of some kind, and we need to extract the data from that */
+		if (public_key) {
+			cert = php_openssl_x509_from_zval(val, 0, &cert_res TSRMLS_CC);
+			free_cert = (cert_res == -1);
+			/* actual extraction done later */
+			if (!cert) {
+				/* not a X509 certificate, try to retrieve public key */
+				BIO* in;
+				if (filename) {
+					in = BIO_new_file(filename, "r");
+				} else {
+					in = BIO_new_mem_buf(Z_STRVAL_PP(val), Z_STRLEN_PP(val));
+				}
+				if (in == NULL) {
+					TMP_CLEAN;
+				}
+				key = PEM_read_bio_PUBKEY(in, NULL,NULL, NULL);
+				BIO_free(in);
+			}
+		} else {
+			/* we want the private key */
+			BIO *in;
+
+			if (filename) {
+				if (php_openssl_safe_mode_chk(filename TSRMLS_CC)) {
+					TMP_CLEAN;
+				}
+				in = BIO_new_file(filename, "r");
+			} else {
+				in = BIO_new_mem_buf(Z_STRVAL_PP(val), Z_STRLEN_PP(val));
+			}
+
+			if (in == NULL) {
+				TMP_CLEAN;
+			}
+			key = PEM_read_bio_PrivateKey(in, NULL,NULL, passphrase);
+			BIO_free(in);
+		}
+	}
+
+	if (public_key && cert && key == NULL) {
+		/* extract public key from X509 cert */
+		key = (EVP_PKEY *) X509_get_pubkey(cert);
+	}
+
+	if (free_cert && cert) {
+		X509_free(cert);
+	}
+	if (key && makeresource && resourceval) {
+		*resourceval = ZEND_REGISTER_RESOURCE(NULL, key, le_key);
+	}
+	if (Z_TYPE(tmp) == IS_STRING) {
+		zval_dtor(&tmp);
+	}
+	return key;
+}
+/* }}} */
+
+
 
 PHP_FUNCTION(shopex_data_encrypt)
 {
