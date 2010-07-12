87c5feef3a4fa2d00c17f95b8eaaba1e10023e8a
xuqinyong@gmail.com <xuqinyong@gmail.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2010-07-12 19:13:05 +0000

    add new

--- trunk/security/crypt/rsa/datasafe/protype/libdatasafe.c
+++ trunk/security/crypt/rsa/datasafe/protype/libdatasafe.c
@@ -87,22 +87,26 @@ RSA *get_shopex_private_key(){
 }
 
 
-void shopex_rsa_encrypt(RSA *pub_rsa,char *input,char *output){	
+void shopex_rsa_encrypt(RSA *pub_rsa,char *input,char **output){	
 	int input_len,rsa_len,buf_num,chunk_len,ret_len,ret_len_total,en_len;
 	char *rsa_ret_buf_p,*rsa_ret_buf;
 	char *ciphertext_p,*ciphertext;
-	char *rsa_input;
+	char *rsa_input,*rsa_input_p;
 	char *b64_buf_p,*b64_buf;
 	char *input_p;
-	
+	char *output_buf;
+
+	ret_len = ret_len_total = 0;
+
 	input_len = strlen(input);
 	rsa_len = RSA_size(pub_rsa);
 	buf_num = input_len / rsa_len + 1;
 	input_p = input;
 	rsa_ret_buf_p = rsa_ret_buf = (char *)malloc(rsa_len * buf_num);
 	ciphertext_p = ciphertext = (char * )malloc(RSA_size(pub_rsa));
+	rsa_input_p = rsa_input = (char *)malloc(110);
 	do{			
-			chunk_len = input_len > 1000 ? 1000 : input_len;
+			chunk_len = input_len > 110 ? 110 : input_len;
 			memcpy(rsa_input,input,chunk_len);
 			//input_len must lower then RSA_size(pub_rsa) - 11			
 			ret_len = RSA_public_encrypt(chunk_len, rsa_input, ciphertext, pub_rsa, RSA_PKCS1_PADDING);
@@ -110,14 +114,17 @@ void shopex_rsa_encrypt(RSA *pub_rsa,char *input,char *output){
 			ret_len_total += ret_len;
 			rsa_ret_buf += ret_len;
 			ciphertext = ciphertext_p;
-			input = input + 1000;
+			rsa_input = rsa_input_p;
+			input = input + 110;
 	}while(input - input_p < input_len );
-	b64_buf_p = b64_buf = (char *)malloc( (ret_len_total * 6 + 7) / 8 );
+	b64_buf_p = b64_buf = (char *)malloc( ret_len_total * 1.5 );
 	base64_encode(rsa_ret_buf_p,ret_len_total,b64_buf,&en_len);
-	output = (char *)malloc(en_len);
-	memcpy(output,b64_buf_p,en_len);
-	output[en_len] = 0;
+	output_buf = (char *)malloc(en_len);
+	memcpy(output_buf,b64_buf_p,en_len);
+	output_buf[en_len] = 0;
 	
+	*output = output_buf; 
+
 	free(b64_buf_p);
 	free(ciphertext_p);
 	free(rsa_ret_buf_p);
@@ -157,13 +164,13 @@ void shopex_rsa_decrypt(RSA *priv_rsa,char *input,char *output){
 void shopex_conf_rsa_encrypt(char *input,char *output ){
 	RSA *pub_rsa;
 	pub_rsa = get_shopex_public_key();
-	shopex_rsa_encrypt(pub_rsa,input,output);	
+	shopex_rsa_encrypt(pub_rsa,input,&output);	
 }
 
 void shopex_conf_rsa_decrypt(char *input,char *output){
 	RSA *priv_rsa;
 	priv_rsa = get_shopex_private_key();
-	shopex_rsa_decrypt(priv_rsa,input,output);	
+//	shopex_rsa_decrypt(priv_rsa,input,output);	
 }
 
 void shopex_data_rsa_encrypt(char *input,char *output){
