6287750aa87c37e47b0ac6a563c51bfbc46f3d13
xuqinyong@gmail.com <xuqinyong@gmail.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2010-07-24 10:08:19 +0000

    reserver

--- security/crypt/rsa/datasafe/get_setting.php
+++ security/crypt/rsa/datasafe/get_setting.php
@@ -7,12 +7,12 @@ $setting = array(
 );
 
 $config_file = "setting.conf";
-$fp = fopen($config_file,"rb+");
+$fp = fopen($config_file,"wb+");
 fwrite($fp,trim($setting[0])."\n");
 fwrite($fp,trim($setting[1])."\n");
 for($i=2;$i<count($setting);$i++){
 	$setting[$i] = trim($setting[$i]);
 	$md5 = md5_file($setting[$i]);
-	fwrite($fp,$setting[5].":".$md5."\n");
+	fwrite($fp,$setting[$i].":".$md5."\n");
 }
 fclose($fp);
