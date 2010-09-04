11dd4b41ae74b1d010a838f6d5055577281cd3ce
xuqinyong@gmail.com <xuqinyong@gmail.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2010-09-04 16:49:32 +0000

    modfiy bash

--- trunk/host/svhost/app/svhost/lib/run_queue.sh
+++ trunk/host/svhost/app/svhost/lib/run_queue.sh
@@ -2,9 +2,13 @@
 HTTPSQS_HOST=192.168.0.2:1218
 QUEUE_NAME=192.168.0.2
 
-curl "http://"$HTTPSQS_HOST"/?charset=utf-8&name="$QUEUE_NAME"&opt=view&pos=1" > tmp.sh
-if [ "$(cat tmp.sh)" != "HTTPSQS_GET_END" ] ; then
-	sh tmp.sh
-	rm -f tmp.sh
-	echo "ok"
-fi
+while true
+do
+	curl "http://"$HTTPSQS_HOST"/?charset=utf-8&name="$QUEUE_NAME"&opt=get&pos=1" > tmp.sh 2>/dev/null
+	if [ "$(cat tmp.sh)" != "HTTPSQS_GET_END" ] ; then
+		sh tmp.sh
+		rm -f tmp.sh
+		echo "ok"
+	fi
+	sleep 1
+done
