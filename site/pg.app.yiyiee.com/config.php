6296853b64d4eaec582eac836038cd716f479fe6
xuqinyong@gmail.com <xuqinyong@gmail.com@8caafd3e-42ec-11de-b17b-0148c022b60b>
2011-01-29 17:11:51 +0000

    pg.app.yiyiee.com

---
+++ site/pg.app.yiyiee.com/config.php
@@ -0,0 +1,6 @@
+<?php
+define("MB_AKEY","aefe2b434ed54bb8bf295a1eb59f841d"); //这里的'*********'请改成你的App Key
+define("MB_SKEY","05d7c7dd63a1328965ab5b391c63f037"); //这里的'*********'请改成你的App Secret
+define( "MB_RETURN_FORMAT" , 'json' );
+define( "MB_API_HOST" , 'open.t.qq.com' );
+?>
